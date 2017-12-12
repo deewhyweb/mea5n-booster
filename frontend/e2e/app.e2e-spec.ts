5cb5622ec7e5368f6da00e71d52651e89773ae35
helio-frota <00hf11@gmail.com>
2017-12-12 09:35:19 -0300

    experiments with travis

--- frontend/e2e/app.e2e-spec.ts
+++ frontend/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('mea5n-booster App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('MEAN Stack Contacts Application');
   });
 });
