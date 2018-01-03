d83e9fdeacd5056cddaefa878264b3546b8b5571
helio-frota <00hf11@gmail.com>
2018-01-03 11:13:11 +0000

    src: allowing any origin to avoid hardcoded URL

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ require('kube-probe')(app);
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
