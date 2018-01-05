03f327e6a5beb3b77f162b071d0cc4e6be7c8f93
helio-frota <00hf11@gmail.com>
2018-01-05 14:56:11 +0000

    src: updating URL for back-end

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://mean-booster-backend-myproject.192.168.42.194.nip.io',
+  apiUrl: 'http://mean-booster-backend-mb.192.168.42.194.nip.io',
 };
